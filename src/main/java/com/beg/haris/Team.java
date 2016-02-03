09c848c7b24e2c171961dce139a566d292853d94
Haris Beg (r625361) <haris.beg@cambiahealth.com>
2016-02-03 15:09:56 -0800

    Part 2 - Return a RESTful Response

---
+++ src/main/java/com/beg/haris/Team.java
@@ -0,0 +1,35 @@
+package com.beg.haris;
+
+public class Team {
+	
+	private Long id;
+	private String name;
+	private String location;
+	private String mascot;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getLocation() {
+		return location;
+	}
+	public void setLocation(String location) {
+		this.location = location;
+	}
+	public String getMascot() {
+		return mascot;
+	}
+	public void setMascot(String mascot) {
+		this.mascot = mascot;
+	}
+
+}
