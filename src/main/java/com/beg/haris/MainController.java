56faa49687cf9dc622de6c410b13e616ba8be4cb
Haris Beg (r625361) <haris.beg@cambiahealth.com>
2016-02-03 14:54:45 -0800

    Part 1 - Simple Web Application

---
+++ src/main/java/com/beg/haris/MainController.java
@@ -0,0 +1,13 @@
+package com.beg.haris;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+
+	@RequestMapping("/")
+	public String hello() {
+		return "hello";
+	}
+}
