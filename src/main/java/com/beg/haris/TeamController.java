066104f88faeb8c9f31e34bf581a0aa7ed14754f
harisbeg <haris.beg@hotmail.com>
2016-02-08 11:24:07 -0800

    Part 4 (Optional)- Create a Single Team endpoint
    Use http://localhost:8080/teams to note the generated ID values for each
    Team. Then use URLs such as http://localhost:8080/teams/1 or
    http://localhost:8080/teams/2 to get results for the individual teams.

--- src/main/java/com/beg/haris/TeamController.java
+++ src/main/java/com/beg/haris/TeamController.java
@@ -1,6 +1,7 @@
 package com.beg.haris;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,5 +13,10 @@ public class TeamController {
 	@RequestMapping("/teams")
 	public Iterable<Team> getTeams() {
 		return teamRepository.findAll();
+	}
+	
+	@RequestMapping("/teams/{id}")
+	public Team getTeam(@PathVariable long id) {
+		return teamRepository.findOne(id);
 	}
 }
